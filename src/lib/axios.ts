be80d0a89c7a0d2d8ab696c9c5182df150802e58
fsouzadi1995 <fsouzadi1995@gmail.com>
2021-12-12 03:55:25 -0300

    chore: update env vars

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,13 +1,10 @@
 import Axios, { AxiosResponse } from 'axios';
-import { Buffer } from 'buffer';
 import { covalentBaseUrl as baseURL } from '../config/static.json';
-
-const encoded = Buffer.from(import.meta.env.VITE_COVALENT_KEY as string).toString('base64');
 
 const axios = Axios.create({
   baseURL,
   params: {
-    key: import.meta.env.VITE_COVALENT_KEY,
+    key: String(import.meta.env.VITE_COVALENT_KEY),
   },
 });
 
