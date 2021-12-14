128c3af28bd4c3385197bcd220b8dffd55bce0a9
fsouzadi1995 <fsouzadi1995@gmail.com>
2021-12-14 06:45:05 -0300

    fix: url normalizer

--- src/utils/string-utils.ts
+++ src/utils/string-utils.ts
@@ -2,9 +2,13 @@ export function shortenAddress(address: string, startLength: number = 5): string
   return `${address.substring(0, startLength)}...${address.substring(address.length - 4)}`;
 }
 
-export function formatToHttpProtocol(url: string) {
+export function normalizeUrl(url: string) {
   if (url.startsWith('ipfs://')) {
     return `https://ipfs.io/ipfs/${url.substring(7)}`;
+  }
+
+  if (url.startsWith('http://')) {
+    return `https://${url.substring(7)}`;
   }
 
   return url;
